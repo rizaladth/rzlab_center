f645d2d5b00076dd661d1830cb94d93391aaa469
RzLab Center <admin@rzlab.id>
2026-07-18 07:07:25 +0700

    fix: add mysql port 3306 for infinityfree

--- rzlab_center/config/koneksi.php
+++ rzlab_center/config/koneksi.php
@@ -6,7 +6,7 @@ define('DB_PASS', 'oaLIH8UOdFY2');
 define('DB_CHARSET', 'utf8mb4');
 
 try {
-    $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
+    $dsn = "mysql:host=" . DB_HOST . ";port=3306;dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
